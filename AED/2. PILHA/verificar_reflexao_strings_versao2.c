cb715cbf25ce7230c15736334891b821e1357d85
Wylys Dev <62449357+wylysdev@users.noreply.github.com>
2022-06-09 14:42:18 -0300

    update

---
+++ AED/2. PILHA/verificar_reflexao_strings_versao2.c
@@ -0,0 +1,18 @@
+int step = 1;
+bool valid = true;
+
+for(char *C = str; valid && *C != '\0'; C++){
+    if(*C == 'A' || *C == 'B'){
+
+        if(step == 1){
+
+            push(stk, *C);
+
+        } else if (step == 2) {
+
+            int s;
+
+        }
+
+    }
+}
